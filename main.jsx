a4096aff98d59742dcc09beee24f1fb25bf54133
koshioi <m.k.koszalka@gmail.com>
2025-06-15 16:39:14 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ConspiracyChat from "./src/ConspiracyChat";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ConspiracyChat />
-  </React.StrictMode>
-);
